26afedfcab90673f51f56b6045ee87f48b0e28d7
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-07 22:28:49 +0200

    feat: mount UAT issue tracker routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -8,6 +8,7 @@ import financeLinkingRouter from "./financeLinking";
 import financeControlRouter from "./financeControl";
 import systemHealthRouter from "./systemHealth";
 import uatTestCenterRouter from "./uatTestCenter";
+import uatIssuesRouter from "./uatIssues";
 import notificationsRouter from "./notifications";
 import managementSummaryRouter from "./managementSummary";
 import dashboardSettingsRouter from "./dashboardSettings";
@@ -34,6 +35,7 @@ router.use("/finance-linking",requireFeature("finance"),requireMenuPermissionByM
 router.use("/finance-control",requireFeature("finance"),requireMenuPermissionByMethod("finance"),financeControlRouter);
 router.use("/system-health",systemHealthRouter);
 router.use("/uat",uatTestCenterRouter);
+router.use("/uat-issues",uatIssuesRouter);
 router.use("/cashier/management-summary",requireFeature("management_dashboard"),requireMenuPermission("finance","can_view_financial"),managementSummaryRouter);
 router.use("/management",requireFeature("management_dashboard"),requireMenuPermission("analytics","can_view_financial"),managementSummaryRouter);
 router.use("/dashboard-settings",requireFeature("management_dashboard"),dashboardSettingsRouter);
