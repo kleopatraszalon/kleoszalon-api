8939637071b3df8a965f6f41f14f23e92f11b44e
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-08 22:11:06 +0200

    Add isolated checklist schema bootstrap

---
+++ src/checklists/ensureChecklists.ts
@@ -0,0 +1,24 @@
+import { readFile } from "fs/promises";
+import path from "path";
+import pool from "../db";
+
+let migrationPromise: Promise<void> | null = null;
+
+export function ensureChecklists() {
+  if (!migrationPromise) {
+    migrationPromise = (async () => {
+      for (const file of [
+        "20260808_CHECKLISTS_V1.sql",
+        "20260808_CHECKLIST_TEST_USERS_V1.sql",
+        "20260808_CHECKLIST_TEST_USERS_V2.sql",
+      ]) {
+        const sqlPath = path.join(__dirname, "..", "sql", file);
+        await pool.query(await readFile(sqlPath, "utf8"));
+      }
+    })().catch((error) => {
+      migrationPromise = null;
+      throw error;
+    });
+  }
+  return migrationPromise;
+}
