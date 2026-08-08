fd8105090779cdaea9369867a61cf0588d0107b6
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-08 15:42:59 +0200

    Bootstrap work order finalization dependencies

--- src/finance/ensureFinanceNav.ts
+++ src/finance/ensureFinanceNav.ts
@@ -2,6 +2,8 @@ import {readFile} from 'fs/promises';
 import path from 'path';
 import pool from '../db';
 import {ensureHrV2} from '../hr/ensureHrV2';
+import {ensureCustomerPortal} from '../customerPortal/ensureCustomerPortal';
+import {ensureMenuHealth} from '../menu/ensureMenuHealth';
 
 let ensurePromise:Promise<void>|null=null;
 
@@ -14,6 +16,7 @@ export function ensureFinanceNav(){
   if(!ensurePromise){
     ensurePromise=(async()=>{
       await ensureHrV2();
+      await ensureCustomerPortal();
       for(const file of [
         '20260807_CASHIER_FINANCIAL_CLOSE_V1.sql',
         '20260807_FINANCE_OPERATIONS_V2.sql',
@@ -22,7 +25,11 @@ export function ensureFinanceNav(){
         '20260808_FINANCE_NAV_SCHEMA_REPAIR_V6.sql',
         '20260808_NAV_ONLINE_INVOICE_V4.sql',
         '20260808_NAV_ONLINE_INVOICE_V5_LIFECYCLE.sql',
+        '20260808_LOYALTY_CASHIER_V5.sql',
+        '20260806_WORKORDER_STOCK_CONSUMPTION_V1.sql',
+        '20260808_WORK_ORDER_FINALIZATION_V2.sql',
       ]) await runSql(file);
+      await ensureMenuHealth();
     })().catch(err=>{ensurePromise=null;throw err});
   }
   return ensurePromise;
