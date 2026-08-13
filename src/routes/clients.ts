2e6d7d473040eafd2fda54a50a5151f54e551b49
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-13 17:20:39 +0200

    Stage17: mount lifecycle administration API

--- src/routes/clients.ts
+++ src/routes/clients.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import clientGovernanceRouter from './clientGovernance';
+import systemHardeningRouter from './systemHardening';
 import clientsCoreRouter from './clientsCore';
 
 const router=Router();
+router.use('/system-hardening',systemHardeningRouter);
 router.use(clientGovernanceRouter);
 router.use(clientsCoreRouter);
 
