42e149d72396ec7b93f2f9b3989c7572e2d6927f
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-18 11:38:06 +0200

    Mount Observability APM center

--- src/routes/notifications.ts
+++ src/routes/notifications.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import legacyNotificationsRouter from "./notificationsLegacy";
 import alertRuleAdminRouter from "./alertRuleAdmin";
+import observabilityRouter from "./observability";
+import { requireManagement } from "../middleware/requireRoles";
 import { startAlertRuleScheduler } from "../services/alertRuleEngine";
+import { startObservabilityWorker } from "../services/observabilityApm";
 
 const router=Router();
 startAlertRuleScheduler();
+startObservabilityWorker();
+router.use("/observability",requireManagement,observabilityRouter);
 router.use("/alert-rules",alertRuleAdminRouter);
 router.use("/",legacyNotificationsRouter);
 export default router;
