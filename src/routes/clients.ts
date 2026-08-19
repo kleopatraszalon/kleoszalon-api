138a53e3365e5a9dc243e2b63351f34a9f9386d2
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-19 13:10:32 +0200

    Mount NBA attribution analytics API

--- src/routes/clients.ts
+++ src/routes/clients.ts
@@ -8,6 +8,7 @@ import marketingAutomationRouter from './marketingAutomation';
 import clientDetailRecoveryRouter from './clientDetailRecovery';
 import customerIntelligenceRouter from './customerIntelligence';
 import nbaMarketingAutomationRouter from './nbaMarketingAutomation';
+import nbaAttributionAdminRouter from './nbaAttributionAdmin';
 import clientsCoreRouter from './clientsCore';
 
 const router=Router();
@@ -18,6 +19,7 @@ router.use(clientGovernanceRouter);
 router.use(clientReceptionContextRouter);
 router.use('/marketing-automation',marketingAutomationRouter);
 router.use(clientDetailRecoveryRouter);
+router.use('/intelligence/attribution',nbaAttributionAdminRouter);
 router.use('/intelligence/marketing',nbaMarketingAutomationRouter);
 router.use('/intelligence',customerIntelligenceRouter);
 router.use(clientsCoreRouter);
