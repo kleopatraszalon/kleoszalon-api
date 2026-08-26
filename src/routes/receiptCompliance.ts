c368f1a4cee59b7527f1ad5f68be7251da92f3b4
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-26 17:11:01 +0200

    Mount receipt issuance endpoints

--- src/routes/receiptCompliance.ts
+++ src/routes/receiptCompliance.ts
@@ -1,1 +1,9 @@
-export { default } from "./receiptComplianceV2";
+import { Router } from "express";
+import receiptComplianceV2 from "./receiptComplianceV2";
+import receiptIssuance from "./receiptIssuance";
+
+const router = Router();
+router.use(receiptComplianceV2);
+router.use(receiptIssuance);
+
+export default router;
