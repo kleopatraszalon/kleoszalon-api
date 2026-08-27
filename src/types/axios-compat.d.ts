595e211a5dc89d52cc225eb06936c4a240ed1285
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-27 11:24:08 +0200

    Use server-local axios compatibility declaration

--- src/types/axios-compat.d.ts
+++ src/types/axios-compat.d.ts
@@ -1,13 +1,4 @@
-import "axios";
-
 declare module "axios" {
-  interface AxiosInstance {
-    get(url: string, config?: any): Promise<any>;
-    post(url: string, data?: any, config?: any): Promise<any>;
-  }
-
-  interface AxiosStatic {
-    get(url: string, config?: any): Promise<any>;
-    post(url: string, data?: any, config?: any): Promise<any>;
-  }
+  const axios: any;
+  export default axios;
 }
